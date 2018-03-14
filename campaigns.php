a72c47a7a8e87990990df56395cf31653ff76840
Nebojsa Nikolic <qawar321@gmail.com>
2018-03-14 13:40:44 +0100

    Change text color

--- campaigns.php
+++ campaigns.php
@@ -18,7 +18,7 @@
                         <i class="fas fa-bell"></i><span>Neu</span>
                     </div>
                     <!--END-->                
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">Army wings</h4>
@@ -30,15 +30,15 @@
                             doloremque error culpa quasi itaque?</p> <!--END-->
                         <hr>
                         
-                        <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
+                        <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
                             <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center"><!-- SCALE -->
                                 <p class="investment-total-amount font-weight-bold m-0 mt-3">220.500&euro;</p>
                                 <p class="investors-number text-muted">22 Investors</p>
                                 <p class="investment-completed">18%</p>
                             </div> <!--END-->  
                             <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
-                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-dark">75.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
-                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-dark">700.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
+                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary">75.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
+                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-secondary">700.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
                             </div> <!--END-->
                         </div>       
                     </div>  
@@ -78,7 +78,7 @@
                         <i class="fas fa-star"></i><span>Favourite</span>
                     </div>
                     <!--END-->
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">Darts heroes (CI/VMn/LW/PP)</h4>
@@ -89,9 +89,9 @@
                             neque eveniet natus, illo debitis dolore dolorem sequi aperiam fuga? Voluptatum velit, repudiandae consequatur
                             doloremque error culpa quasi itaque?</p> <!--END-->
                         <hr>
-                        <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->    
+                        <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->    
                             <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
-                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-dark text-center">450.000<sup class="currency">&euro;</sup></span> Capital Investment</p>
+                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary text-center">450.000<sup class="currency">&euro;</sup></span> Capital Investment</p>
                             </div> <!--END-->
                         </div>       
                     </div>  
@@ -117,7 +117,7 @@
                     <div class="campaign-badge bg-warning">
                         <i class="fas fa-star"></i><span>Favourite</span>
                     </div><!--END-->
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">M office ICO</h4>
@@ -125,7 +125,7 @@
                         <!-- SHORT SUMMARY - EXCERT -->
                         <p class="campaign-excerpt card-text mt-3">Phasellus vehicula risus nec lacus facilisis, eu condimentum turpis tincidunt. Ut ullamcorper est ut bibendum volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin ut consectetur felis. Aenean facilisis metus non fermentum ultricies. Donec in aliquam lacus, eu porttitor tortor. Maecenas facilisis pharetra elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p> <!--END-->
                         <hr>
-                        <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
+                        <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
                             <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center"><!-- SCALE -->
                                 <p class="investment-type font-weight-bold m-0 mt-3">AHT</p>
                                 <p class="investment-total-amount font-weight-bold m-0">1.00</p>
@@ -134,9 +134,9 @@
                             </div> <!--END-->  
                             <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
                                 <span class="d-block text-muted mb-1">HERO token</span>
-                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-dark">75.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
+                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary">75.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
                                 <span class="d-block text-muted mb-1">HERO token</span>
-                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-dark">700.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
+                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-secondary">700.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
                             </div> <!--END-->
                         </div>       
                     </div>  
@@ -147,11 +147,11 @@
                             </div>
                             <div class="info-cell mx-1 px-2 py-2 text-center">
                                 <span class="d-block text-muted font-weight-bold">Value in $</span>
-                                <span class="d-block text-dark font-weight-bold">0.00</span>
+                                <span class="d-block text-secondary font-weight-bold">0.00</span>
                             </div>
                             <div class="info-cell mx-1 px-2 py-2 text-center">
                                 <span class="d-block text-muted font-weight-bold">Value in &euro;</span>
-                                <span class="d-block text-dark font-weight-bold">0.00</span>
+                                <span class="d-block text-secondary font-weight-bold">0.00</span>
                             </div>
                         </div> <!--END-->         
                         <div class="campaign-invest-info py-1 px-3 border-bottom border-muted">
@@ -191,7 +191,7 @@
                     <div class="campaign-badge bg-danger">
                         <i class="fas fa-heart"></i><span>We love this</span>
                     </div><!--END-->                
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/che.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">Intechor travel</h4>
@@ -199,15 +199,15 @@
                         <!-- SHORT SUMMARY - EXCERT -->
                         <p class="campaign-excerpt card-text mt-3">Etiam congue nisl et libero semper placerat. Integer tincidunt ac est id facilisis. Vestibulum suscipit dictum augue eu tempus. Donec dignissim luctus pulvinar. Integer at quam dapibus, scelerisque justo id, ultrices massa. Suspendisse lacinia dui a viverra aliquet. Suspendisse potenti. Nam nec dolor sodales, luctus massa ac, varius est. Aenean mattis, orci non ultrices porta, nulla nulla feugiat neque, vel interdum mi mi sed purus. Mauris iaculis lectus dui, a dignissim odio ornare non. Aenean scelerisque magna a turpis efficitur blandit. Phasellus et ultricies diam. Sed vitae accumsan turpis. Cras euismod, diam nec dapibus dapibus, lectus lectus ultricies quam, id varius dolor mi vel lectus. </p> <!--END-->
                         <hr>
-                        <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
+                        <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
                             <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center"><!-- SCALE -->
                                 <p class="investment-total-amount font-weight-bold m-0 mt-3">311.500&euro;</p>
                                 <p class="investors-number  text-muted">7 Investors</p>
                                 <p class="investment-completed">565%</p>
                             </div> <!--END-->  
                             <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
-                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-dark">55.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
-                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-dark">550.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
+                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary">55.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
+                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-secondary">550.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
                             </div> <!--END-->
                         </div>       
                     </div>  
@@ -242,7 +242,7 @@
                     <div class="campaign-badge bg-soon">
                         <i class="fas fa-bullhorn"></i><span> Startet bald</span>
                     </div><!--END-->                
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">Makit - ein regionales Produkt</h4>
@@ -252,12 +252,12 @@
                         <div class="campaign-failed p-2 d-flex flex-row justify-content-around border-bottom border-muted"><!--THIS SECTION ONLY IF CAMPAIGN FAILS-->
                             <div class="investment-amounts align-self-center pt-3">
                                 <p class="text-muted">
-                                    <span class="investment-min d-block h3 m-0 text-dark">
+                                    <span class="investment-min d-block h3 m-0 text-secondary">
                                         0<sup class="currency">&euro;</sup>
                                     </span> Supports
                                 </p>
                                 <p class="text-muted">
-                                    <span class="investment-max h4 m-0 text-dark">0</span> Supporter
+                                    <span class="investment-max h4 m-0 text-secondary">0</span> Supporter
                                 </p>
                             </div>
                             <div class="completed-wrapper my-auto d-inline-block rounded-circle bg-info p-3 text-white text-center">
@@ -286,7 +286,7 @@
                     <div class="campaign-badge bg-success">
                         <i class="fas fa-rocket"></i></i><span> Erfolgreich finanziert</span>
                     </div> <!--END-->                
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">Oceanic travel</h4>
@@ -299,12 +299,12 @@
                         <div class="campaign-failed p-2 d-flex flex-row justify-content-around border-bottom border-muted"><!--THIS SECTION ONLY IF CAMPAIGN FAILS-->
                             <div class="investment-amounts align-self-center pt-3">
                                 <p class="text-muted">
-                                    <span class="investment-min d-block h3 m-0 text-dark">
+                                    <span class="investment-min d-block h3 m-0 text-secondary">
                                         63.000<sup class="currency">&euro;</sup>
                                     </span> Supported
                                 </p>
                                 <p class="text-muted">
-                                    <span class="investment-max h4 m-0 text-dark">10</span> Supporters
+                                    <span class="investment-max h4 m-0 text-secondary">10</span> Supporters
                                 </p>
                             </div>
                             <div class="completed-wrapper my-auto d-inline-block rounded-circle bg-info p-3 text-white text-center">
@@ -343,7 +343,7 @@
                     <div class="campaign-badge bg-danger">
                         <i class="fas fa-heart"></i><span>We love this!</span>
                     </div> <!--END-->                
-                    <div class="campaign-content card-body text-dark bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
+                    <div class="campaign-content card-body text-secondary bg-light"><!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->       
                         <div class="card-title d-flex justify-content-start align-items-center"><!-- TITLE WITH COUNTRY FLAG -->
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
                             <h4 class="campaign-title mb-0">Medix (CF)</h4>
@@ -355,15 +355,15 @@
                             doloremque error culpa quasi itaque?</p> <!--END-->
                         <hr>
                         
-                        <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
+                        <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
                             <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center"><!-- SCALE -->
                                 <p class="investment-total-amount font-weight-bold m-0 mt-3">76.500&euro;</p>
                                 <p class="investors-number  text-muted">121 Investors</p>
                                 <p class="investment-completed">225%</p>
                             </div> <!--END-->  
                             <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
-                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-dark">35.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
-                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-dark">180.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
+                                <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary">35.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
+                                <p class="text-muted"><span class="investment-max d-block h4 m-0 text-secondary">180.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
                             </div> <!--END-->
                         </div>       
                     </div>  
