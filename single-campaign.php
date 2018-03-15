0126698f301970ae8189ba2fe43ab1575f39cc7f
Nebojsa Nikolic <qawar321@gmail.com>
2018-03-15 14:12:17 +0100

    Fix single campaign cover

--- single-campaign.php
+++ single-campaign.php
@@ -2,62 +2,90 @@
         <main>
             <!--single campaign heading-->
             <section class="container">
-                <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex justify-content-between align-items-center flex-wrap mb-5">
                     <div class="d-flex align-items-center my-5">
                         <div>
                             <img class="rounded-circle border border-primary w-100" src="images/campaigns/rsz_anoodeling_logo.jpg" alt="">
                         </div>
-                        <div class="pl-3 d-flex">
-                            <h3 class="m-0 company-name">ANOODELING<h3>
+                        <div class="pl-3 d-flex flex-wrap">
+                            <h3 class="m-0 company-name">ANOODELING</h3>
                             <div class="d-flex align-items-center pl-2">
-                                <div class="pr-3"><img class="flag" src="images/flags/aut.jpg" alt=""></div>
+                                <div class="pr-3 d-flex align-items-center"><img class="flag-wide" src="images/flags/aut.jpg" alt=""></div>
                                 <h4 class="m-0">Wien, Austria</h4>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <button type="button" class="btn btn-success btn-lg">INVESTIEREN</button> 
+                    <div class="d-block d-sm-flex justify-content-center mb">
+                        <a href="#" class="btn btn-success btn-lg">INVESTIEREN</a> 
                     </div>
                 </div>
             </section>
             <!--single campaign cover-->
             <section class="single-campain-cover bg-info">
-                <div class="container">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <h1 class="company-name">Anoodeling</h1>
-                            <p class="company-basic-info">Lorem ipsum dolor sit amet, cetero nonumes consetetur est eu, sea graece lucilius incorrupte an, in euismod deleniti his. Vidit offendit phaedrum ea eos, sea paulo omittam eu, quis tollit ea sit. In cum nulla movet urbanitas, falli copiosae in quo. Usu utamur meliore ea, justo ancillae te mei. Vis aperiam euripidis eloquentiam te.</p>
+                <div class="container h-100">
+                    <div class="row h-100">
+                        <div class="col-12 col-lg-7 text-white">
+                            <h1 class="company-name flex-wrap">Anoodeling(CI/ AFG/ BC/ AN) </h1>
+                            <div class="d-flex">
+                                <hr class="bg-white w-25">
+                                <div class="w-25"></div>
+                                <div class="w-25"></div>
+                                <div class="w-25"></div>
+                            </div>
+                            <p class="company-basic-info font-italic">Lorem ipsum dolor sit amet, cetero nonumes consetetur est eu, sea graece lucilius incorrupte an, in euismod deleniti his. Vidit offendit phaedrum ea eos, sea paulo omittam eu, quis tollit ea sit. In cum nulla movet urbanitas, falli copiosae in quo. Usu utamur meliore ea, justo ancillae te mei. Vis aperiam euripidis eloquentiam te.</p>
                         </div>
-                        <div class="container bg-white w-100">
-                            <h5 class="font-weight-light text-muted py-3">STARTET BALD</h5>
-                            <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
+                        <div class="col-5 d-none d-lg-block bg-white">
+                            <div class="container">
+                                <h5 class="font-weight-light text-muted py-3">STARTET BALD</h5>
+                                <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
+                                    <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center"><!-- SCALE -->
+                                        <p class="investment-total-amount font-weight-bold m-0 mt-3">220.500&euro;</p>
+                                        <p class="investors-number text-muted">22 Investors</p>
+                                        <p class="investment-completed">18%</p>
+                                    </div> <!--END-->  
+                                    <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
+                                        <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary">500.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
+                                        <p class="text-muted"><span class="investment-max d-block h4 m-0 text-secondary">2.000.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
+                                    </div> <!--END-->
+                                </div>
+                                <div class="campaign-invest-info py-1 d-flex flex-wrap justify-content-around border-bottom border-top border-muted mb-5 mt-3"><!--ADDITIONAL INFORMATION DISPLAYED AS ICONS WITH TOOLTIPS-->
+                                    <div class="info-cell mx-1 px-2 py-2 d-flex flex-row">
+                                        <i class="fas fa-users text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="KMU"></i>
+                                    </div>
+                                    <div class="info-cell mx-1 px-2 py-2 text-center">
+                                        <i class="fas fa-calendar-alt text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="Vertragslaufzeit"></i>
+                                    </div>
+                                </div> <!--END-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--hidden campaign cover item-->
+            <section>
+                <div class="container">
+                    <div class="hidden-data d-block d-lg-none bg-white">
+                        <div class="container">
+                            <h5 class="font-weight-light text-muted py-3 text-center">STARTET BALD</h5>
+                            <div class="campaign-invest-wrapper text-secondary d-flex flex-column flex-sm-row justify-content-around"><!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->   
                                 <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center"><!-- SCALE -->
                                     <p class="investment-total-amount font-weight-bold m-0 mt-3">220.500&euro;</p>
                                     <p class="investors-number text-muted">22 Investors</p>
                                     <p class="investment-completed">18%</p>
                                 </div> <!--END-->  
                                 <div class="investment-amounts align-self-center pt-3"><!-- BASIC INFO ABOUT INVESTMENTS -->
-                                    <p class="text-muted"><span class="investment-min d-block h4 m-0 text-dark">75.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
-                                    <p class="text-muted"><span class="investment-max d-block h4 m-0 text-dark">700.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
+                                    <p class="text-muted"><span class="investment-min d-block h4 m-0 text-secondary">500.000<sup class="currency">&euro;</sup></span> Minimum Investment</p>
+                                    <p class="text-muted"><span class="investment-max d-block h4 m-0 text-secondary">2.000.000<sup class="currency">&euro;</sup></span> Maximum investment</p>
                                 </div> <!--END-->
                             </div>
-                            <div class="campaign-invest-info py-2 my-4 d-flex flex-wrap justify-content-around border-top border-bottom border-muted"><!--ADDITIONAL INFORMATION DISPLAYED AS ICONS WITH TOOLTIPS-->
+                            <div class="campaign-invest-info py-1 d-flex flex-wrap justify-content-around border-bottom border-top border-muted mb-5 mt-3"><!--ADDITIONAL INFORMATION DISPLAYED AS ICONS WITH TOOLTIPS-->
                                 <div class="info-cell mx-1 px-2 py-2 d-flex flex-row">
-                                    <i class="fas fa-users text-muted"></i>
+                                    <i class="fas fa-users text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="KMU"></i>
                                 </div>
                                 <div class="info-cell mx-1 px-2 py-2 text-center">
-                                    <i class="far fa-calendar text-muted"></i>
+                                    <i class="fas fa-calendar-alt text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="Vertragslaufzeit"></i>
                                 </div>
                             </div> <!--END-->
-                            <!--
-                            <div class="d-flex flex-wrap justify-content-around pb-3">
-                                <div class="campaign-info-item d-flex flex-column flex-wrap justify-content-center align-items-center">
-                                    <i class="fas fa-users text-muted"></i>
-                                </div>
-                                <div class="campaign-info-item d-flex flex-column flex-wrap justify-content-center align-items-center">
-                                    <i class="far fa-calendar text-muted"></i>
-                                </div>
-                            </div>-->
                         </div>
                     </div>
                 </div>
@@ -277,9 +305,9 @@
                                     <div class="company-details mb-3">
                                         <h3 class="border-bottom pb-3 mb-3">Company details</h3>
                                         <div><i class="fas fa-map-marker text-muted pr-2"></i><span class="text-muted">Bruno-Kreisky-Platz 1, 1220 Wien, Wien, Austria</span></div>
-                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="#" class="text-success">+1086438517</a></div>
-                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="#" class="text-success">office@anoodeling.com</a></div>
-                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="#" class="text-success">www.anoodeling.com</a></div>
+                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="tel:+1086438517" class="text-success">+1086438517</a></div>
+                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="mailto:office@anoodeling.com" class="text-success">office@anoodeling.com</a></div>
+                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="www.anoodeling.com" class="text-success">www.anoodeling.com</a></div>
                                     </div>
                                 </aside>
                             </div>
@@ -331,10 +359,10 @@
                                             <i class="mb-3 text-success">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</i>
                                         </blockquote>
                                     </div>
-                                    <div class="side-nav">
+                                    <!-- <div class="side-nav">            URADITI KAKO SE VALJA^ JEDARED KADA NAUCIS KAKO TO URADITI VALJA^NO
                                         <a href="#anoodeling" class="btn rounded-circle side-nav-item" data-toggle="tooltip" data-placement="right" title="Anoodeling"></a>
                                         <a href="#investieren" class="btn rounded-circle side-nav-item" data-toggle="tooltip" data-placement="right" title="Investieren"></a>
-                                    </div>
+                                    </div> -->
                                 </div>
                                 <!--aside-->
                                 <aside class="col-12 col-lg-4 campaign-side mb-3">
@@ -386,9 +414,9 @@
                                     <div class="company-details mb-3">
                                         <h3 class="border-bottom pb-3 mb-3">Company details</h3>
                                         <div><i class="fas fa-map-marker text-muted pr-2"></i><span class="text-muted">Bruno-Kreisky-Platz 1, 1220 Wien, Wien, Austria</span></div>
-                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="#" class="text-success">+1086438517</a></div>
-                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="#" class="text-success">office@anoodeling.com</a></div>
-                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="#" class="text-success">www.anoodeling.com</a></div>
+                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="tel:+1086438517" class="text-success">+1086438517</a></div>
+                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="mailto:office@anoodeling.com" class="text-success">office@anoodeling.com</a></div>
+                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="www.anoodeling.com" class="text-success">www.anoodeling.com</a></div>
                                     </div>
                                 </aside>
                             </div>
@@ -430,9 +458,9 @@
                                     <div class="company-details mb-3">
                                         <h3 class="text-muted border-bottom font-weight-normal pb-3 mb-3">Company details</h3>
                                         <div><i class="fas fa-map-marker text-muted pr-2"></i><span class="text-muted">Bruno-Kreisky-Platz 1, 1220 Wien, Wien, Austria</span></div>
-                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="#" class="text-success">+1086438517</a></div>
-                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="#" class="text-success">office@anoodeling.com</a></div>
-                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="#" class="text-success">www.anoodeling.com</a></div>
+                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="tel:+1086438517" class="text-success">+1086438517</a></div>
+                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="mailto:office@anoodeling.com" class="text-success">office@anoodeling.com</a></div>
+                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="www.anoodeling.com" class="text-success">www.anoodeling.com</a></div>
                                     </div>
                                 </aside>
                             </div>
@@ -519,9 +547,9 @@
                                     <div class="company-details mb-3">
                                         <h3 class="text-muted border-bottom font-weight-normal pb-3 mb-3">Company details</h3>
                                         <div><i class="fas fa-map-marker text-muted pr-2"></i><span class="text-muted">Bruno-Kreisky-Platz 1, 1220 Wien, Wien, Austria</span></div>
-                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="#" class="text-success">+1086438517</a></div>
-                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="#" class="text-success">office@anoodeling.com</a></div>
-                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="#" class="text-success">www.anoodeling.com</a></div>
+                                        <div><i class="fas fa-phone text-muted pr-2"></i><a href="tel:+1086438517" class="text-success">+1086438517</a></div>
+                                        <div><i class="fas fa-envelope text-muted pr-2"></i><a href="mailto:office@anoodeling.com" class="text-success">office@anoodeling.com</a></div>
+                                        <div><i class="fas fa-link text-muted pr-2"></i><a href="www.anoodeling.com" class="text-success">www.anoodeling.com</a></div>
                                     </div>
                                 </aside>
                             </div>
